94f2439e2b6cd83837be5cc7c7cc0272816f09d9
baochi0212 <chideptraiak29@gmail.com>
2023-04-10 06:20:44 +0000

    ...

--- UIT-MCAN/train.py
+++ UIT-MCAN/train.py
@@ -98,7 +98,7 @@ def main():
                             specials=["<pad>", "<sos", "<eos>"])
     train_image_dir = config.train_image_dir
     test_image_dir = config.test_image_dir
-    train_dataset = ViVQA(config.json_overfit_path, vocab, train_image_dir)
+    train_dataset = ViVQA(config.json_train_path, vocab, train_image_dir)
     metrics.vocab = vocab
     # for i in range(len(train_dataset)):
     #     train_dataset[i]
